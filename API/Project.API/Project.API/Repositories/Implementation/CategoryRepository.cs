71e830d598070c594109b0d1ed3470cbb82afcdd
zdrobix <zdroba88@gmail.com>
2025-03-04 21:13:50 +0200

    Created interface and implementation for repository pattern.

---
+++ API/Project.API/Project.API/Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.API.Data;
+using Project.API.Models.Domain;
+using Project.API.Repositories.Interface;
+
+namespace Project.API.Repositories.Implementation
+{
+	public class CategoryRepository : ICategoryRepository
+	{
+		private readonly ApplicationDbContext dbContext;
+
+		public CategoryRepository(ApplicationDbContext dbContext) =>
+			this.dbContext = dbContext;
+        public async Task<Category> CreateAsync(Category category)
+		{
+			await dbContext.Categories.AddAsync(category);
+			await dbContext.SaveChangesAsync();
+
+			return category;
+		}
+		public async Task<IEnumerable<Category>> GetAllAsync() => 
+			await dbContext.Categories.ToListAsync();
+		
+	}
+}
---
+++ API/Project.API/Project.API/Repositories/Interface/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Project.API.Models.Domain;
+
+namespace Project.API.Repositories.Interface
+{
+	public interface ICategoryRepository
+	{
+		Task<Category> CreateAsync(Category category);
+
+		Task<IEnumerable<Category>> GetAllAsync();
+	}
+}
