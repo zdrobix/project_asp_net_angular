6ce3c2f0f0f3c48578cf6816462dad88e51fe02b
zdrobix <zdroba88@gmail.com>
2025-03-04 21:14:28 +0200

    Controller for categories, add and get all methods.

---
+++ API/Project.API/Project.API/Controllers/CategoriesController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project.API.Data;
+using Project.API.Models.Domain;
+using Project.API.Models.DTO;
+using Project.API.Repositories.Interface;
+
+namespace Project.API.Controllers
+{
+	// https://localhost:5001/api/categories
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
+	{
+		private readonly ICategoryRepository categoryRepository;
+
+		public CategoriesController(ICategoryRepository categoryRepository)
+        {
+			this.categoryRepository = categoryRepository;
+		}
+
+        //
+        [HttpPost] 
+		public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+		{
+			// dto -> domain model
+			var category = new Category
+			{
+				Name = request.Name,
+				UrlHandle = request.UrlHandle
+			};
+
+			await categoryRepository.CreateAsync(category);
+
+			// domain model -> dto
+			var response = new CategoryDto
+			{
+				Id = category.Id,
+				Name = category.Name,
+				UrlHandle = category.UrlHandle
+			};
+
+			return Ok(response);
+		}
+
+		// GET: https://localhost:7179/api/Categories
+		[HttpGet]
+		public async Task<IActionResult> GetCategories()
+		{
+			var categories = await categoryRepository.GetAllAsync();
+
+			// domain model -> dto
+			var response = categories.Select(category => new CategoryDto
+			{
+				Id = category.Id,
+				Name = category.Name,
+				UrlHandle = category.UrlHandle
+			});
+
+			return Ok(response);
+		}
+	}
+}
