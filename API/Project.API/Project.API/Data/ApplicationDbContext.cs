19e74813360429829145017bd2578b4f84bbf1f0
zdrobix <zdroba88@gmail.com>
2025-03-04 21:14:04 +0200

    Created database context.

---
+++ API/Project.API/Project.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.API.Models.Domain;
+
+namespace Project.API.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+	}
+}
