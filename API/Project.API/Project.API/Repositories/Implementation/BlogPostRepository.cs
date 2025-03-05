cc07e270a31713c2c4c014369a5dcc937b1b8614
zdrobix <zdroba88@gmail.com>
2025-03-05 18:39:12 +0200

    Created repo + interface for blogposts.

---
+++ API/Project.API/Project.API/Repositories/Implementation/BlogPostRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Project.API.Data;
+using Project.API.Models.Domain;
+using Project.API.Repositories.Interface;
+
+namespace Project.API.Repositories.Implementation
+{
+	public class BlogPostRepository : IBlogPostRepository
+	{
+		private readonly ApplicationDbContext dbContext;
+
+		public BlogPostRepository(ApplicationDbContext dbContext)
+        {
+			this.dbContext = dbContext;
+		}
+
+        public async Task<BlogPost> CreateAsync(BlogPost blogPost)
+		{
+			await dbContext.BlogPosts.AddAsync(blogPost);
+			await dbContext.SaveChangesAsync();
+
+			return blogPost;
+		}
+	}
+}
---
+++ API/Project.API/Project.API/Repositories/Interface/IBlogPostRepository.cs
@@ -0,0 +1,9 @@
+﻿using Project.API.Models.Domain;
+
+namespace Project.API.Repositories.Interface
+{
+	public interface IBlogPostRepository
+	{
+		Task<BlogPost> CreateAsync(BlogPost blogPost);
+	}
+}
