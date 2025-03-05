e214ff324020a70c8a1e2929696d9f0b67130c46
zdrobix <zdroba88@gmail.com>
2025-03-05 17:22:17 +0200

    Repository methods for get by id, update and delete.

--- API/Project.API/Project.API/Repositories/Implementation/CategoryRepository.cs
+++ API/Project.API/Project.API/Repositories/Implementation/CategoryRepository.cs
@@ -18,8 +18,38 @@ namespace Project.API.Repositories.Implementation
 
 			return category;
 		}
+
 		public async Task<IEnumerable<Category>> GetAllAsync() => 
 			await dbContext.Categories.ToListAsync();
-		
+
+		public async Task<Category?> GetById(Guid id) =>
+			await dbContext.Categories.FirstOrDefaultAsync(category => category.Id == id);
+
+		public async Task<Category?> UpdateAsync(Category category)
+		{
+			var existingCategory = await dbContext.Categories.FirstOrDefaultAsync(x => x.Id == category.Id);
+
+			if (existingCategory != null)
+			{
+				dbContext.Entry(existingCategory).CurrentValues.SetValues(category);
+				await dbContext.SaveChangesAsync();
+				return category;
+			}
+
+			return null;
+		}
+
+		public async Task<Category?> DeleteAsync(Guid id)
+		{
+			var existingCategory = await dbContext.Categories.FirstOrDefaultAsync(category => category.Id == id);
+
+			if (existingCategory == null)
+				return null;
+
+			dbContext.Categories.Remove(existingCategory);
+			await dbContext.SaveChangesAsync();
+
+			return existingCategory;
+		}
 	}
 }
