4ac2d91e3999370f56b53fc2b24f491fc3ec264e
zdrobix <zdroba88@gmail.com>
2025-03-15 10:31:03 +0200

    Markdown import.

--- UI/project/src/app/app.module.ts
+++ UI/project/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
 import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
 import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { EditBlogpostComponent } from './features/blog-post/edit-blogpost/edit-blogpost.component';
 
 @NgModule({
   declarations: [
@@ -21,12 +23,14 @@ import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blog
     EditCategoryComponent,
     BlogpostListComponent,
     AddBlogpostComponent,
+    EditBlogpostComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MarkdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
