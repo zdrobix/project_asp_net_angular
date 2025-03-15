9374d889c50d0fbcc9f23cc304c83bfda6d7d845
zdrobix <zdroba88@gmail.com>
2025-03-15 10:33:59 +0200

    Blogpost list component.

--- UI/project/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ UI/project/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogPostService } from '../services/blog-post.service';
+import { Observable } from 'rxjs/internal/Observable';
+import { BlogPost } from '../models/blogpost.model';
 
 @Component({
   selector: 'app-blogpost-list',
   templateUrl: './blogpost-list.component.html',
   styleUrls: ['./blogpost-list.component.css']
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit {
 
+  blogPosts$?: Observable<BlogPost[]>
+  
+  constructor(private blogPostService: BlogPostService) { 
+  }
+
+  ngOnInit(): void {
+     this.blogPosts$ = this.blogPostService.getAllBlogPosts();
+  }
 }
