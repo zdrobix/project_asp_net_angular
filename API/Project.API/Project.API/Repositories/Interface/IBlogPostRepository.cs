2e6082928a8e6255f54e6462c97f120ef5ad44d5
zdrobix <zdroba88@gmail.com>
2025-03-15 10:28:22 +0200

    Get all, get by id interface functions.

--- API/Project.API/Project.API/Repositories/Interface/IBlogPostRepository.cs
+++ API/Project.API/Project.API/Repositories/Interface/IBlogPostRepository.cs
@@ -5,5 +5,7 @@ namespace Project.API.Repositories.Interface
 	public interface IBlogPostRepository
 	{
 		Task<BlogPost> CreateAsync(BlogPost blogPost);
+		Task<IEnumerable<BlogPost>> GetAllAsync();
+		Task<BlogPost?> GetByIdAsync(Guid id);
 	}
 }
