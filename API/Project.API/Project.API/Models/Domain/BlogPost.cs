968d37a4b3269011fca497e54ccb2029a3cbe65b
zdrobix <zdroba88@gmail.com>
2025-03-15 10:25:23 +0200

    Fixed type in field name, added categories field.

--- API/Project.API/Project.API/Models/Domain/BlogPost.cs
+++ API/Project.API/Project.API/Models/Domain/BlogPost.cs
@@ -1,4 +1,6 @@
-﻿namespace Project.API.Models.Domain
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Project.API.Models.Domain
 {
 	public class BlogPost
 	{
@@ -6,10 +8,11 @@
         public string Title { get; set; }
         public string ShortDescription { get; set; }
         public string Content { get; set; }
-        public string FeatureImageUrl { get; set; }
+        public string FeaturedImageUrl { get; set; }
         public string UrlHandle { get; set; }
         public DateTime PublishedDate { get; set; }
         public string Author { get; set; }
         public bool IsVisible { get; set; }
+        public ICollection<Category> Categories { get; set; }
     }
 }
